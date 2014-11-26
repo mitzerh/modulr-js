6d01408b5ab9d8f3b5a34a8c03b113cd33059fab
Helcon Mabesa <helcon.mabesa@foxnews.com>
2014-11-26 14:22:21 -0500

    cleanup

--- src/modulr.js
+++ src/modulr.js
@@ -19,7 +19,8 @@
         });
 
         var isOpera = (typeof opera !== 'undefined' && opera.toString() === '[object Opera]') ? true : false,
-            readyRegExp = /^(complete|loaded)$/;
+            readyRegExp = /^(complete|loaded)$/,
+
 
         var Modulr = function(CONFIG) {
 
@@ -45,10 +46,11 @@
                 INSTANCE_INIT = false,
                 INSTANCE_READY = false;
 
-            // version
-            this.version = "${version}";
-
             var Proto = this;
+
+            // version
+            Proto.version = "${version}";
+
 
             /**
              * get current instance's config
@@ -87,7 +89,7 @@
              */
             Proto.require = function(deps, callback) {
 
-                if (isStr(deps)) {
+                if (typeof deps === "string") {
 
                     return getDefinedModule(deps);
 
@@ -192,21 +194,7 @@
                     callback();
                 };
 
-                // dependency loader for other instances
-                // if (CONFIG.instanceDeps) {
-
-                //     loadInstanceDeps(CONFIG.instanceDeps, function(){
-                //         INSTANCE_READY = true;
-                //         callback();
-                //     });
-
-                // } else {
-
-                //     INSTANCE_READY = true;
-                //     callback();
-
-                // }
-                
+                // load shim
                 loadShim(function(){
                     isReady();
                 });
@@ -491,7 +479,7 @@
          */
         function getFactory(factory, deps) {
             var ret = null;
-            if (isFN(factory)) {
+            if (typeof factory === "function") {
                 ret = factory.apply(factory, deps);
             } else {
                 ret = factory;
@@ -570,18 +558,6 @@
 
         function rtrimSlash(val) {
             return (val.charAt(val.length - 1) === "/") ? val.slice(0, val.length - 1) : val;
-        }
-
-        function isStr(val) {
-            return (typeof val === "string") ? true : false;
-        }
-
-        function isFN(val) {
-            return (typeof val === "function") ? true : false;
-        }
-
-        function isObj(val) {
-            return (typeof val === "object" && !isArray(val)) ? true : false;
         }
 
         function isArray(val) {
